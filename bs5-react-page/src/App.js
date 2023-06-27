f08d6f92772af133e578a0a28cda7db911342a01
Mustafa Eideh <mustafaeideh100@outlook.com>
2023-06-27 14:45:34 -0300

    Project finish.

--- bs5-react-page/src/App.js
+++ bs5-react-page/src/App.js
@@ -1,24 +1,108 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from "react-bootstrap";
+import { Card, Container, Nav, Navbar, NavDropdown, Row, Col, Image, Button } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
+
+      <main>
+        <Container>
+          <Row className="px-4 my-5">
+            <Col>
+              <Image
+                src="holder.js/100px250"
+                fluid
+                rounded
+                className=''
+              />;
+            </Col>
+            <Col>
+              <h1 class='font-weigh-light'>Tagline</h1>
+              <p class='mt-4'>This is a template that is great for small busnisses.It doesn`t have too much fancy flare to it, but it makes a great  use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+              <Button variant="outline-primary">Call to action</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Card className='text-center bg-secondary text-white my-5 py-4'>
+              <Card.Body>This is some text within a card body.</Card.Body>
+            </Card>
+          </Row>
+          <Row>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+
+      <footer class="py-5 my-5 bg-dark">
+        <Container className='px-4'>
+          <p class='text-center text-white'>Copyright &copy; Your website 2023</p>
+        </Container>
+      </footer>
     </div>
   );
 }
